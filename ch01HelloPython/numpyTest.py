f48969314cd322fb149f25a6f34711683e733846
Taemin Kim <taemin3000@naver.com>
2017-04-26 16:30:11 +0900

    numpyTest

---
+++ ch01HelloPython/numpyTest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 16:22:07 2017
+
+@author: taemi
+"""
+
+import numpy as np
+
+x = np.array([1.0,2.0,3.5])
+print(x)
+print(type(x))
+
+y = np.array([2.0,3.0,4.7])
+print(x-y)
+print(x+y)
+print(x/y)
+print(x*y)
+print(x%y)
+print(x**2)
